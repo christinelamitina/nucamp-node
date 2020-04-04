d67ba173732755a1899f0517ccf2d2788bc55e4e
Christine Lamitina <christinelamitina@gmail.com>
2020-04-04 10:17:58 -0500

    Week 4 Workshop Task 1

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1708506359288974',
+        clientSecret: '139b32ecd2bf1b94dbddd853edf27dee'
+    }
 }
